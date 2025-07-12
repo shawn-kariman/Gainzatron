22219077f507cf5d1a9e8d70cd1af12e742463a4
Shawn Kariman <shahriar.kariman@gmail.com>
2025-07-12 14:49:41 -0300

    Learned some Tailwind.

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -1,21 +1,16 @@
-<div class="stack">
+<div class="stack bg-gradient-to-br from-orange-500 via-orange-700 to-teal-500">
     <div class="background"></div>
-    <h1>Gainzatron</h1>
+    <h1 class="place-self-center text-6xl bg-gradient-to-br from-orange-500 via-orange-700 to-teal-500 bg-clip-text text-transparent">Gainzatron</h1>
 </div>
 
 <style lang="postcss">
     .stack {
-        background: linear-gradient(45deg, orange, teal);
+        /* background: linear-gradient(45deg, orange, teal); */
         height: 50svh;
     }
     .background{
         margin: 1rem;
         border-radius: 1rem;
         background: linear-gradient( 90deg, #202020, #282828);
-    }
-    h1 {
-        color: papayawhip;
-        font-size: 3rem;
-        place-self: center;
     }
 </style>

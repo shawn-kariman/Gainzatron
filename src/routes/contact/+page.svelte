d71c6a2ddeabaadc139d240d093b49254d55627c
Shawn Kariman <shahriar.kariman@gmail.com>
2025-07-21 16:52:32 -0300

    Cntact Route is being made

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Button from "$lib/components/ui/Button.svelte"
+
+</script>
+<h1
+    class="text-6xl"
+>Contact</h1>
+
+<p>The best way to get in touch with us is through our growing discord community.</p>
+
+<Button href="">Join the Discord</Button>
